557e21be0aa3bad0148afe600683772be33790ee
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-13 16:04:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,9 +33,9 @@ end
 def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
   
-  emoticon_hash.each do |english_word, emoticon_set| 
-    if emoticon_hash[english_word][:japanese] == emoticon 
-      return emoticon_hash[english_word][:english][emoticon_set.first]
+  emoticon_hash.each do |emoticon_set| 
+    if emoticon_hash[english_word][:japanese] == [emoticon_set.last]
+      return emoticon_hash[english_word]
     end
   end
   
